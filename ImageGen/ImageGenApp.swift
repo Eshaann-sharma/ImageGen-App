a493425a852b5f2a4fc593fb7fcf96fabfa46eab
Eshaann-sharma <eshaansharma2244@gmail.com>
2024-09-16 10:44:25 +0530

    Initial Commit

---
+++ ImageGen/ImageGenApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageGenApp.swift
+//  ImageGen
+//
+//  Created by Eshaan Sharma on 16/09/24.
+//
+
+import SwiftUI
+
+@main
+struct ImageGenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
